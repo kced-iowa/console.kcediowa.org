7ae44fe185e6777f48d0eca5bbef879996184de0
horsaen <atealltheglue@protonmail.com>
2022-09-04 17:28:50 -0500

    navbar stuff

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -5,15 +5,44 @@ import styles from './Dashboard.module.css';
 import { CgProfile } from 'react-icons/cg';
 
 function Dashboard() {
+
+    const buttons = [
+        {
+            title: "Home",
+            link: "dashboard"
+        },
+        {
+            title: "Members",
+            link: "members"
+        },
+        {
+            title: "Companies",
+            link: "companies"
+        },
+        {
+            title: "Orgsanizations",
+            link: "orgs"
+        },
+        {
+            title: "Locations",
+            link: "locations"
+        }
+    ];
     return (
         <div>
             <Head>
                 <title>Dashboard | SADC Console</title>
             </Head>
             <div className={styles.navbar}>
-                <div className={styles.button}>
-
-                </div>
+                {buttons.map(({title, link}) => (
+                    <Link href={'/' + link} key="">
+                        <a>
+                            <div className={styles.button}>
+                                <span>{title}</span>
+                            </div>
+                        </a>
+                    </Link>
+                ))}
                 <div className={styles.account}>
                     <CgProfile className={styles.space} />
                     <span>HORSAEN</span>
